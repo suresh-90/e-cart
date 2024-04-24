1ff9c16c63c39c249f111d622199484572d06f09
suresh-90 <168008075+suresh-90@users.noreply.github.com>
2024-04-24 21:36:54 +0530

    Add files via upload

---
+++ MainBody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../MainBody/MainBody.css'
+import Cards from '../MainBody/Cards/Cards.js'
+
+export default function MainBody() {
+  return (
+    <div className='main'>
+      <Cards/>
+    </div>
+  )
+}
